fcb47b2f8bf5a4a5d7892fd2aee1cf1ec543047b
김규석 <kyu05017@naver.com>
2024-01-01 16:19:58 +0900

    설정값 YML 이관

--- src/main/java/com/hayeum/frontserver/FrontServerApplication.java
+++ src/main/java/com/hayeum/frontserver/FrontServerApplication.java
@@ -3,12 +3,20 @@ package com.hayeum.frontserver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @Slf4j
 @SpringBootApplication
 public class FrontServerApplication {
+
+	public static final String APPLICATION_LOCATIONS =
+			"spring.config.location="
+					+ "classpath:application.yml,"
+					+ "classpath:/yml/serverSetting.yml";
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(FrontServerApplication.class, args);
+		new SpringApplicationBuilder(FrontServerApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 		log.info("********** Start Front Server **********");
 		log.info("At : [{}]",System.currentTimeMillis());
 		log.info("****************************************");
